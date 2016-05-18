78fa3e0fcf2123e3487c948d3834d60e77cd94fc
Andrew Nagy <andrew.nagy@the159.com>
2016-05-17 17:28:30 -0700

    translation and visualization

--- views/grid.php
+++ views/grid.php
@@ -21,8 +21,8 @@
 			<button id="remove-all" class="btn btn-danger btn-remove" data-type="extensions" disabled data-section="all">
 				<i class="glyphicon glyphicon-remove"></i> <span><?php echo _('Delete')?></span>
 			</button>
-			<span class="btn btn-default disabled">
-				<b>Server time:</b>
+			<span class="btn btn-time disabled">
+				<b><?php echo _("Server time")?>:</b>
 				<div id="servertime" data-time="<?php echo time()?>" data-zone="<?php echo date("e")?>" style="display: inline;"><span><?php echo _("Not received")?></span></div>
 			</span>
 		</div>
