af566f48fddeab500222f8d041949434417aba54
James Finstrom <jfinstrom@sangoma.com>
2018-07-10 22:19:40 +0000

    Cleanup backup classes

--- Backup.php
+++ Backup.php
@@ -1,11 +1,11 @@
 <?php
-namespace FreePBX\modules\__MODULENAME__;
+namespace FreePBX\modules\Hotelwakeup;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
   public function runBackup($id,$transaction){
     $configs = [
-        'config' = $this->FreePBX->Hotelwakeup->getSetting(),
-        'calls' = $this->FreePBX->Hotelwakeup->getAllCalls(),
+        'config' => $this->FreePBX->Hotelwakeup->getSetting(),
+        'calls' => $this->FreePBX->Hotelwakeup->getAllCalls(),
     ];
     $this->addDependency('ivr');
     $this->addConfigs($configs);
