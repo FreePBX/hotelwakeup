d16a1d3a710d81f6c862b77bf1cf7afdd9b2369a
James Finstrom <jfinstrom@sangoma.com>
2018-07-17 18:27:53 +0000

    Restore cleanup

--- Restore.php
+++ Restore.php
@@ -3,7 +3,7 @@ namespace FreePBX\modules\Hotelwakeup;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-    $configs = $this->getConfig();
+    $configs = reset($this->getConfigs());
     $this->FreePBX->Hotelwakeup->saveSetting($configs['config']);
     foreach ($configs['calls'] as $call) {
         $this->FreePBX->Hotelwakeup->addWakeup($call['destination'], $call['time'], $call['lang']);
