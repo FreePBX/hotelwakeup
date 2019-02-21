133ac8053eefdd3e395270f1a101566d7d110d3f
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 17:23:20 -0500

    [Module Tag script: hotelwakeup 15.0.4] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -2,29 +2,17 @@
 namespace FreePBX\modules\Hotelwakeup;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = reset($this->getConfigs());
-    $this->processConfigs($configs);
-  }
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    $tables = array_flip($tables + $unknownTables);
-    if (!isset($tables['hotelwakeup'])) {
-      return $this;
-    }
-    $cb = $this->FreePBX->Hotelwakeup;
-    $cb->setDatabase($pdo);
-    $configs = [
-      'config' => $cb->getSetting(),
-      'calls' => $cb->getAllCalls(),
-    ];
-    $cb->resetDatabase();
-    $this->processConfigs($configs);
-    return $this;
-  }
-  public function processConfigs($configs){
-    $this->FreePBX->Hotelwakeup->saveSetting($configs['config']);
-    foreach ($configs['calls'] as $call) {
-      $this->FreePBX->Hotelwakeup->addWakeup($call['destination'], $call['time'], $call['lang']);
-    }
-  }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		$this->processConfigs($configs);
+	}
+	public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
+		$this->restoreLegacyDatabase($pdo);
+	}
+	public function processConfigs($configs){
+		$this->FreePBX->Hotelwakeup->saveSetting($configs['config']);
+		foreach ($configs['calls'] as $call) {
+			$this->FreePBX->Hotelwakeup->addWakeup($call['destination'], $call['time'], $call['lang']);
+		}
+	}
 }
